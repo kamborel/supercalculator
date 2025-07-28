d90983c08e527a612cce449f3716bc093a266fcc
kamborel <57349675+kamborel@users.noreply.github.com>
2025-07-28 18:01:31 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/kamborel/supercalculator/super_calculator/cmd"
+import "github.com/kamborel/super_calculator/super_calculator/cmd"
 
 func main() {
 	cmd.Execute()
